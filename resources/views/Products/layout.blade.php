adbe386559a32a6a2d7a3262c21704552c0d697d
Folakunmi Aremu <folakunmi.aremu@princepsfinance.com>
2021-05-21 16:46:56 +0100

    first commit

---
+++ resources/views/Products/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>AWS TEST</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+</head>
+<body style="margin-top:30px ">
+
+<div class="container margin-tb">
+    @yield('content')
+</div>
+
+</body>
+</html>
